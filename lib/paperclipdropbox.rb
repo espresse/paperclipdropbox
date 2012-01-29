8847785877c81ce9f830a3775994306415e9b05c
unknown <Lajfbuk@Lajfbuk-laptop.(none)>
2012-01-29 15:45:49 +0100

    Using dropbox_sdk api iso dropbox

--- lib/paperclipdropbox.rb
+++ lib/paperclipdropbox.rb
@@ -8,23 +8,24 @@ module Paperclip
     extend self
     
 			def self.extended(base)
-				require "dropbox"
+				require "dropbox_sdk"
 				base.instance_eval do
 					
 					@dropbox_key = '8ti7qntpcysl91j'
 					@dropbox_secret = 'i0tshr4cpd1pa4e'
 					@dropbox_public_url = "http://dl.dropbox.com/u/"
-					@options.url ="#{@dropbox_public_url}#{user_id}#{@options.path}"
-					@url = @options.url
-					@path = @options.path
+					@options[:url] ="#{@dropbox_public_url}#{user_id}#{@options[:path]}"
+					@url = @options[:url]
+					@path = @options[:path]
 					log("Starting up DropBox Storage")
+					log(@options[:url])
 				end
 			end
 
 			def exists?(style = default_style)
 				log("exists?  #{style}") if respond_to?(:log)
 				begin
-					dropbox_session.metadata("/Public#{File.dirname(path(style))}")
+					dropbox_client.metadata("/Public#{File.dirname(path(style))}")
 					log("true") if respond_to?(:log)
 					true
 				rescue
@@ -35,24 +36,26 @@ module Paperclip
 
 			def to_file(style=default_style)
 				log("to_file  #{style}") if respond_to?(:log)
-				return @queued_for_write[style] || "#{@dropbox_public_url}#{user_id}/#{path(style)}"
+				return @queued_for_write[style] || "#{@dropbox_public_url}#{user_id}#{path(style)}"
 			end
 
 			def flush_writes #:nodoc:
 				log("[paperclip] Writing files #{@queued_for_write.count}")
 				@queued_for_write.each do |style, file|
 					log("[paperclip] Writing files for ") if respond_to?(:log)
-					file.close
-					dropbox_session.upload(file.path, "/Public#{File.dirname(path(style))}", :as=> File.basename(path(style)))
+					myfile = file.open
+					response = dropbox_client.put_file("/Public#{File.dirname(path(style))}/#{File.basename(path(style))}", myfile)
 				end
 				@queued_for_write = {}
 			end
 
 			def flush_deletes #:nodoc:
+				p "dfdfd"
 				@queued_for_delete.each do |path|
 					log("[paperclip] Deleting files for #{path}") if respond_to?(:log)
 					begin
-						dropbox_session.rm("/Public/#{path}")
+						#dropbox_session.rm("/Public/#{path}")
+						dropbox_client.file_delete("/Public/#{path}")
 					rescue
 					end
 				end
@@ -60,13 +63,15 @@ module Paperclip
 			end
 
 			def user_id
-				unless Rails.cache.exist?('DropboxSession:uid')
+				unless Rails.cache.exist?('DropboxClientUid')
 					log("get Dropbox Session User_id")
-					Rails.cache.write('DropboxSession:uid', dropbox_session.account.uid)
-					dropbox_session.account.uid
+					
+					client = DropboxClient.new(dropbox_session, :dropbox)
+					Rails.cache.write('DropboxClientUid', client.account_info()['uid'])
+					client.account_info()['uid']
 				else
 					log("read Dropbox User_id") if respond_to?(:log)
-					Rails.cache.read('DropboxSession:uid')
+					Rails.cache.read('DropboxClientUid')
 				end
 			end
 
@@ -75,14 +80,25 @@ module Paperclip
 					if @dropboxsession.blank?
 						log("loading session from yaml") if respond_to?(:log)
 						if File.exists?("#{Rails.root}/config/dropboxsession.yml")
-							@dropboxsession = Dropbox::Session.deserialize(File.read("#{Rails.root}/config/dropboxsession.yml"))
+							@dropboxsession = DropboxSession.deserialize(File.read("#{Rails.root}/config/dropboxsession.yml"))
 						end
 					end
-					@dropboxsession.mode = :dropbox unless @dropboxsession.blank?
+					#@dropboxsession.mode = :dropbox unless @dropboxsession.blank?
 					@dropboxsession
 				else
 					log("reading Dropbox Session") if respond_to?(:log)
 					Rails.cache.read('DropboxSession')
+				end
+			end
+
+			def dropbox_client
+				unless Rails.cache.exist?('DropboxClient')
+					if @dropboxclient.blank?
+						@dropboxclient = DropboxClient.new(dropbox_session, :dropbox)
+					end
+					@dropboxclient
+				else
+					Rails.cache.read('DropboxClient')
 				end
 			end
 		end
